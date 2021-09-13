cd89c3c702b68ddf98352b1f0e556a260cd0cc44
JMuckian94 <johnmuckian@gmail.com>
2021-09-13 13:57:15 +0000

    Created flask app routes

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 import os
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-if os.path.exists("env.py"):
-    import env
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+# if os.path.exists("env.py"):
+#     import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/member")
+def member():
+    return render_template("member.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
